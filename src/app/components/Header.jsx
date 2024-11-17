f38e81255b376a2958710d8d2ff386aa660cc8e9
kenogenyi <ogenyiken@gmail.com>
2024-11-17 16:44:35 +0100

    add next -theme/dark mode

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,32 +5,9 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { usePathname } from 'next/navigation';
 import { useTheme } from 'next-themes';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-import { dark, light } from '@clerk/themes';
-import { useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
 export default function Header() {
   const path = usePathname();
   const { theme, setTheme } = useTheme();
-  const router = useRouter();
-  const [searchTerm, setSearchTerm] = useState('');
-  const searchParams = useSearchParams();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const urlParams = new URLSearchParams(searchParams);
-    urlParams.set('searchTerm', searchTerm);
-    const searchQuery = urlParams.toString();
-    router.push(`/search?${searchQuery}`);
-  };
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(searchParams);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    if (searchTermFromUrl) {
-      setSearchTerm(searchTermFromUrl);
-    }
-  }, [searchParams]);
   return (
     <Navbar className='border-b-2'>
       <Link
@@ -38,18 +15,16 @@ export default function Header() {
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          Sahand&apos;s
+          kenswag's
         </span>
         Blog
       </Link>
-      <form onSubmit={handleSubmit}>
+      <form>
         <TextInput
           type='text'
           placeholder='Search...'
           rightIcon={AiOutlineSearch}
           className='hidden lg:inline'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
@@ -64,21 +39,11 @@ export default function Header() {
         >
           {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
-        <SignedIn>
-          <UserButton
-            appearance={{
-              baseTheme: theme === 'light' ? light : dark,
-            }}
-            userProfileUrl='/dashboard?tab=profile'
-          />
-        </SignedIn>
-        <SignedOut>
-          <Link href='/sign-in'>
-            <Button gradientDuoTone='purpleToBlue' outline>
-              Sign In
-            </Button>
-          </Link>
-        </SignedOut>
+        <Link href='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue' outline>
+            Sign In
+          </Button>
+        </Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
