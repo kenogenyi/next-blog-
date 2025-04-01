57a0d2c79ab7535240dd24156de474e2fba3b80e
kenogenyi <ogenyiken@gmail.com>
2025-04-01 07:23:15 +0100

    cloudinary

--- src/app/dashboard/create-post/page.jsx
+++ src/app/dashboard/create-post/page.jsx
@@ -32,9 +32,11 @@ export default function CreatePostPage() {
             />
             <Select>
               <option value='uncategorized'>Select a category</option>
-              <option value='javascript'>JavaScript</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
+              <option value='javascript'>Football</option>
+              <option value='reactjs'>Movies</option>
+              <option value='nextjs'>Music</option>
+              <option value='nextjs'>Health</option>
+              <option value='nextjs'>Others</option>
             </Select>
           </div>
           <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
