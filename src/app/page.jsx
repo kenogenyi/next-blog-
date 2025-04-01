13a6e7377b1036ceaf837aba1cca67b3739ba180
kenogenyi <ogenyiken@gmail.com>
2025-04-01 07:34:00 +0100

    cloud

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>fuck me</div>
+    <div>home</div>
   )
 }
 
---
+++ src/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+
+
+const cloudinaryconfig = {
+
+    cloud_name: "<dpc6fqwyd>",
+    apikey: "<847156497818263>",
+    apisecret: "<uMA7VX1LeCd3w8TY6i7KwoAtrFg>",
+    secure_distribution: "<ml_default>",
+    private_cdn: "<false>",
+    api_host: "<api.cloudinary.com>",
+    upload_presente: "<upload>",
+    api_url: "<https://api.cloudinary.com/v1_1/dpc6fqwyd/image/upload>",
+    upload_url: "<https://api.cloudinary.com/v1_1/dpc6fqwyd/image/upload>",
+}
+
+export default cloudinaryconfig;
+
+
+
+
+
+
